4f0c635ba9c9c398ca04fc6e2adbf426bef0096d
Gisele Pecapedra <gisele.bernardes@winedirect.com>
2022-09-13 11:25:05 -0300

    feat: landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,51 @@
+import { MagnifyingGlassPlus } from "phosphor-react";
+import logoImg from "./assets/logo.svg";
+import "./styles/main.css";
+
 function App() {
-  return <>Hello World</>;
+  return (
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logoImg} alt="" />
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu{" "}
+        <span className="text-transparent bg-nlw-gradient bg-clip-text">
+          duo
+        </span>{" "}
+        está aqui.
+      </h1>
+
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        <a href="#" className="relative rounded-lg overflow-hidden">
+          <img src="" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Title</strong>
+            <span className="text-zinc-300 text-sm block mt-1">
+              qtd anuncios
+            </span>
+          </div>
+        </a>
+      </div>
+
+      <div className="pt-1 mt-8 bg-nlw-gradient self-stretch rounded-lg overflow-hidden">
+        <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
+          <div>
+            <strong className="text-2xl text-white font-black block">
+              Não encontrou seu duo?
+            </strong>
+            <span className="text-zinc-400 block">
+              Publique um anúncio para encontrar novos players!
+            </span>
+          </div>
+
+          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
+            <MagnifyingGlassPlus size={24} />
+            Publicar anuncio
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
